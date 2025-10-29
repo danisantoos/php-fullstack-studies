9d14a30bdc80e8b05b821dbf5c476f0ddd220ecb
Dani Santos <santos.danielle2173@gmail.com>
2025-10-28 23:33:42 -0300

    Explica case sensitive com exemplos.

---
+++ 2_conceitos_basicos/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    // não é case sensitive!
+    echo "teste <br>";
+    eChO "testando 2 <br>";
+    ECHO "testando 3 <br>";  
+
+    // é case sensitive 
+    $nome = "Danielle";
+    $NOME = "teste";
+
+    echo $nome;
+    echo "<br>";
+    echo $NOME;
