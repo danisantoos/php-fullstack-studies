a513a981dcb665cd4f94bdf199c7ff2162dfe7ae
Dani Santos <santos.danielle2173@gmail.com>
2025-10-29 18:30:51 -0300

    Adicionando explicação do uso de intrução de codigo php

---
+++ 2_conceitos_basicos/2_instrucao_de_codigo/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    // uma linha usa-se ponto e virgula (;)
+ echo "Uma linha <br>";
+ echo "Outra linha <br>";
+
+ // multi linha usa-se chaves ({})
+ if (5 > 2 ) {
+    echo "Dentro do if";
+ }
