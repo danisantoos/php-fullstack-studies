fbdb9b43c3bd52a0a6b50817489f5951cd616fe5
Dani Santos <santos.danielle2173@gmail.com>
2025-11-05 19:04:58 -0300

    Estudos: trabalhando com números float, verificações com is_float() e exercícios práticos

---
+++ 3_tipos_de_dados/4_float/index.php
@@ -0,0 +1,13 @@
+<?php
+
+  $a = 1.12;
+
+  echo $a;
+  echo "<br>";
+  // se colocar um número com (,) ele vai entender que é apenas um valor e vai juntar todos, por isso usa-se o (.) em floats.
+
+  echo 1,123;
+  echo "<br>";
+  echo 12.5 + 1.3278;
+  echo "<br>";
+  echo 12 + 1.3278;
