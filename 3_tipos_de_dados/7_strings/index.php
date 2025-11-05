268d54d11357b447147ba0cd2d2ad8e27a2deaa9
Dani Santos <santos.danielle2173@gmail.com>
2025-11-05 19:48:35 -0300

    Estudos: manipulação de strings em PHP — exemplos, verificações e exercícios práticos

---
+++ 3_tipos_de_dados/7_strings/index.php
@@ -0,0 +1,11 @@
+<?php
+
+  echo "Testando texto de aspas duplas <br>";
+   echo 'Testando texto de aspas simples <br>';
+   echo "Ele disse: 'Olá!' <br> ";
+   echo 'Ele disse: "Olá!" <br> ';
+
+   // Aspas duplas interpretam variáveis dentro de uma string.
+
+   $idade = 15;
+   echo "Ele tem $idade anos";
