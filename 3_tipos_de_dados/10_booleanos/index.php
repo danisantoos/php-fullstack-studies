9e1ae8851c89f9c10ed61f440af6a0c9daa9c566
Dani Santos <santos.danielle2173@gmail.com>
2025-11-07 20:10:04 -0300

    Aprendizado sobre o tipo booleano em PHP — exemplos práticos e explicações

---
+++ 3_tipos_de_dados/10_booleanos/index.php
@@ -0,0 +1,22 @@
+<?php
+  
+  // Quando imprimi o valor (true) em booleano, aparece o número 1 enquanto false é (0) só usamos eles apenas para alguma condição (if).
+
+  echo true;
+  echo "<br>";
+  echo false; 
+
+  if(true) {
+    echo "É verdadeiro! <br>";
+  }
+
+   if(5 > 2) {
+    echo "É verdadeiro! <br>";
+  } 
+
+  $podeEntrar = true;
+
+  if($podeEntrar) {
+    echo "O usuário pode entrar!";
+  }
+
