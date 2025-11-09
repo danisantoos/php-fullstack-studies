d18d91267ad4c8ec3eaa6c65e389f64313d08a3e
Dani Santos <santos.danielle2173@gmail.com>
2025-11-09 19:58:43 -0300

    Novo módulo concluído: arrays e arrays associativos em PHP 🧩

---
+++ 3_tipos_de_dados/15_exercico_7/index.php
@@ -0,0 +1,15 @@
+<?php
+
+ $pessoa = [
+    'nome' => 'Matheus',
+    'idade' => 29,
+    'profissao' => 'programador',
+    'graduacao' => 'Sistemas da informação'
+ ]; 
+
+  $maioridade = 18;
+
+  // desafio
+  if($pessoa ['idade'] >= $maioridade){
+    echo "A pessoa é maior de idade!";
+  }
